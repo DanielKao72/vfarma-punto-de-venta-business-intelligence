a514c64a8c82d58e082e6a32bd847e89d1dcc13c
Pablo Ernesto Baeza Lara <a19201679@alumnos.uady.mx>
2024-10-08 22:59:25 -0600

    build: :construction: Creación clases relacionadas con productos
    Creación de clases relacionadas con el modelo de los productos a manipular

---
+++ ventas/src/main/java/com/vfarma/Modelo/Carritocompras.java
@@ -0,0 +1,23 @@
+package com.vfarma.Modelo;
+
+import java.util.ArrayList;
+
+public class Carritocompras {
+    private ArrayList<Producto> listaProductos = new ArrayList<>();
+
+    public void agregarProducto(Producto producto) {
+        listaProductos.add(producto);
+    }
+
+    public void eliminarProducto(Producto producto) {
+        listaProductos.remove(producto);
+    }
+
+    public void vaciarLista() {
+        listaProductos.clear();
+    }
+
+    public ArrayList<Producto> obtenerListaProductos() {
+        return listaProductos;
+    }
+}
