c009e2f77030880d4349448d22cd881e208c0a9e
Rodri1712 <a19201489@alumnos.uady.mx>
2024-10-27 14:11:17 -0600

    Creando clases y definiendo variables y métodos

---
+++ ventas/src/main/java/com/vfarma/RegistroEmpleados/RecursosHumanos.java
@@ -0,0 +1,10 @@
+package com.vfarma.RegistroEmpleados;
+
+public class RecursosHumanos {
+
+    public void agregarNuevoEmpleado(){}
+    public void transferirEmpleado(){}
+    public void editarEmpleado(){}
+    public void eliminarEmpleado(){}
+
+}
---
+++ ventas/src/main/java/com/vfarma/RegistroEmpleados/VistaRegistroEmpleados.java
@@ -0,0 +1,9 @@
+package com.vfarma.RegistroEmpleados;
+
+public class VistaRegistroEmpleados {
+
+    public void mostrarVistaListaUsuarios(){}
+    public void mostrarVistaFormularioUsuario(){}
+    public void mostrarVistaDetallesUsuario(){}
+    
+}
