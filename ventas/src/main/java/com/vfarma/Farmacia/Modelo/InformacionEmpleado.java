76eed73978e48a0a916dc61bdc382142316bb7ad
Juan Daniel Kao Pech <a19201340@alumnos.uady.mx>
2024-11-22 08:50:32 -0600

    feat: :sparkles: agregar ADT Informacion del Empleado

---
+++ ventas/src/main/java/com/vfarma/Farmacia/Modelo/InformacionEmpleado.java
@@ -0,0 +1,88 @@
+package com.vfarma.Farmacia.Modelo;
+
+public class InformacionEmpleado {
+
+    private String claveEmpleado;
+    private String nombreEmpleado;
+    private String apellidoEmpleado;
+    private String correoEmpleado;
+    private String telefonoEmpleado;
+    private String sexoEmpleado;
+    private String turnoEmpleado;
+    private String rolEmpleado;
+
+    public InformacionEmpleado(String clave, String nombre, String apellido, String correo, String telefono, String sexo, String turno, String rol) {
+        this.claveEmpleado = clave;
+        this.nombreEmpleado = nombre;
+        this.apellidoEmpleado = apellido;
+        this.correoEmpleado = correo;
+        this.telefonoEmpleado = telefono;
+        this.sexoEmpleado = sexo;
+        this.turnoEmpleado = turno;
+        this.rolEmpleado = rol;
+    }
+
+    public String obtenerClaveEmpleado(){
+        return this.claveEmpleado;
+    }
+    
+    public String obtenerNombreEmpleado(){
+        return this.nombreEmpleado;
+    }
+
+    public String obtenerApellidoEmpleado(){
+        return this.apellidoEmpleado;
+    }
+
+    public String obtenerCorreoEmpleado(){
+        return this.correoEmpleado;
+    }
+
+    public String obtenerTelefonoEmpleado(){
+        return this.telefonoEmpleado;
+    }
+
+    public String obtenerSexoEmpleado(){
+        return this.sexoEmpleado;
+    }
+
+    public String obtenerTurnoEmpleado(){
+        return this.turnoEmpleado;
+    }
+
+    public String obtenerRolEmpleado(){
+        return this.rolEmpleado;
+    }
+
+    public void colocarClaveEmpleado(String clave){
+        this.claveEmpleado = clave;
+    }
+
+    public void colocarNombreEmpleado(String nombre){
+        this.nombreEmpleado = nombre;
+    }
+
+    public void colocarApellidoEmpleado(String apellido){
+        this.apellidoEmpleado = apellido;
+    }
+
+    public void colocarCorreoEmpleado(String correo){
+        this.correoEmpleado = correo;
+    }
+
+    public void colocarTelefonoEmpleado(String telefono){
+        this.telefonoEmpleado = telefono;
+    }
+
+    public void colocarSexoEmpleado(String sexo){
+        this.sexoEmpleado = sexo;
+    }
+
+    public void colocarTurnoEmpleado(String turno){
+        this.turnoEmpleado = turno;
+    }
+
+    public void colocarRolEmpleado(String rol){
+        this.rolEmpleado = rol;
+    }
+}
